9a899074c4d87abc91681381d5bea83151104ee7
Safaa <Safaaalnhal7@gmail.com>
2026-04-20 10:12:38 +0300

    feat: implement search API integration

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -9,6 +9,7 @@ export class UsersService {
   const response = await fetch(`https://api.github.com/users/${username}`);
    
   const data = await response.json();
+   console.log('Fetched user:', data.login);
 
   return data;
 }
